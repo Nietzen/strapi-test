1d6d682b3fd6a5b1b180c803514d2a335f07abc5
Jose Segura <joseg@thehangar.cr>
2020-10-17 18:41:24 -0600

    debug

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => {
         settings: {
           client: env('DATABASE_CLIENT'),
           host: env('DATABASE_HOST'),
-          port: env('DATABASE_CLIENT'),
+          port: env('DATABASE_PORT'),
           username: env('DATABASE_USERNAME'),
           password: env('DATABASE_PASSWORD'),
           database: env('DATABASE_NAME'),
