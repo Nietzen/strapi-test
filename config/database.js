e962ba33014d2ebd9a469da81368851aaa8925cc
Jose Segura <joseg@thehangar.cr>
2020-10-19 08:58:27 -0600

    s3

--- config/database.js
+++ config/database.js
@@ -1,27 +1,21 @@
-module.exports = ({ env }) => {
-  const config = {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: env('DATABASE_CLIENT'),
-          host: env('DATABASE_HOST'),
-          port: env('DATABASE_PORT'),
-          username: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          database: env('DATABASE_NAME'),
-          schema: "public"
-        },
-        options: {
-          useNullAsDefault: true,
-          debug: env('APP_DEBUG')
-        },
+module.exports = ({env}) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: env('DATABASE_CLIENT'),
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME'),
+        schema: "public"
+      },
+      options: {
+        useNullAsDefault: true,
+        debug: env('APP_DEBUG')
       },
     },
-  };
-
-  console.log("config", config.connections.default.settings);
-
-  return config;
-};
+  },
+});
